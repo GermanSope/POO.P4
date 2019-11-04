daa62630085e59f99975990ac985f423ca5780e8
Chiquitoxx <sope.german@gmail.com>
2019-11-04 20:56:33 -0300

    services 1.0

--- src/main/java/ar/edu/unnoba/poo/practica4/services/EventService.java
+++ src/main/java/ar/edu/unnoba/poo/practica4/services/EventService.java
@@ -18,9 +18,37 @@ public class EventService {
     private EventRepository EventRepository;
     
     public List<Event> retrieveAllEvents() {
-		return EventRepository.findAll();
+		  return EventRepository.findAll();
     }
     
     public void addEvent(Event event) {
 	    EventRepository.save(event);
-	}
+    }
+
+   public Event getEvent(Long id) {
+    return EventRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+   }
+
+   public Event replaceEvent(Event event, Long id) {
+    return eventRepository.findById(id)
+      .map(e -> {
+        e.setEventDate(event.getEventDate());
+        e.setStartRegistrations(event.getStartRegistrations());
+        e.setEndRegistrations(event.getEndRegistrations());
+        e.setCapacity(event.getCapacity());
+        e.setCost(event.getCost());
+        e.setPrivateEvent(event.getPrivateEvent)
+        return eventRepository.save(e);
+      })
+      .orElseGet(() -> {
+        return eventRepository.save(event);
+      });
+
+    public void deleteEvent(@PathVariable Long id) {
+      eventRepository.deleteById(id);
+    }
+     
+    public Event findEventById(long id) {
+      return eventRepository.findEventById(username);
+    }
+}
---
+++ src/main/java/ar/edu/unnoba/poo/practica4/services/InviteService.java
@@ -0,0 +1,39 @@
+package ar.edu.unnoba.poo.practica4.services;
+
+import java.util.List;
+
+import ar.edu.unnoba.poo.practica4.repositories.InviteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+import ar.edu.unnoba.poo.practica4.entities.Invite;
+
+@Service
+public class InviteService {
+    @Autowired
+    private InviteRepository InviteRepository;
+    
+    public List<Invite> retrieveInvite() {
+		return inviteRepository.findAll();
+    }
+    
+    public void addInvite(Invite invite) {
+	    inviteRepository.save(invite);
+    }
+    
+    public Invite getInvite(Long id) {
+	    return InviteRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+    }
+    
+    
+    public void deleteInvite(@PathVariable Long id) {
+		inviteRepository.deleteById(id);
+    }
+    
+    public Invite findInviteById (Long id) {
+		return inviteRepository.findInviteById (Id);
+	}
+}
