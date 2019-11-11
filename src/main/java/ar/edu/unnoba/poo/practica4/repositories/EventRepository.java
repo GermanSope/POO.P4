ec431248166e54c99bd92f423b7d679789ef1b63
Chiquitoxx <sope.german@gmail.com>
2019-11-11 17:53:13 -0300

    putoelqlee

--- src/main/java/ar/edu/unnoba/poo/practica4/repositories/EventRepository.java
+++ src/main/java/ar/edu/unnoba/poo/practica4/repositories/EventRepository.java
@@ -3,5 +3,10 @@ package ar.edu.unnoba.poo.practica4.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ar.edu.unnoba.poo.practica4.entities.Event;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("select e from Event e where e.id = :id")
+	public Event findEventById(@Param("id") long id);
 }
