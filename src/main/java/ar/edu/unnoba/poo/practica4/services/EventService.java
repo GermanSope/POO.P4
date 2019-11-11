ab897d6ee95d48f6d7b6437301437678229dbcc6
VanneyFranco <francovanney83@gmail.com>
2019-11-11 17:54:05 -0300

    Todo piola

--- src/main/java/ar/edu/unnoba/poo/practica4/services/EventService.java
+++ src/main/java/ar/edu/unnoba/poo/practica4/services/EventService.java
@@ -3,6 +3,8 @@ package ar.edu.unnoba.poo.practica4.services;
 import java.util.List;
 
 import ar.edu.unnoba.poo.practica4.repositories.EventRepository;
+import ar.edu.unnoba.poo.practica4.repositories.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.stereotype.Service;
@@ -15,40 +17,41 @@ import ar.edu.unnoba.poo.practica4.entities.Event;
 public class EventService {
 
     @Autowired
-    private EventRepository EventRepository;
+    private EventRepository eventRepository;
     
     public List<Event> retrieveAllEvents() {
-		  return EventRepository.findAll();
+		  return eventRepository.findAll();
     }
     
     public void addEvent(Event event) {
-	    EventRepository.save(event);
+	    eventRepository.save(event);
     }
 
    public Event getEvent(Long id) {
-    return EventRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+    return eventRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
    }
 
    public Event replaceEvent(Event event, Long id) {
-    return eventRepository.findById(id)
-      .map(e -> {
-        e.setEventDate(event.getEventDate());
-        e.setStartRegistrations(event.getStartRegistrations());
-        e.setEndRegistrations(event.getEndRegistrations());
-        e.setCapacity(event.getCapacity());
-        e.setCost(event.getCost());
-        e.setPrivateEvent(event.getPrivateEvent)
-        return eventRepository.save(e);
-      })
-      .orElseGet(() -> {
-        return eventRepository.save(event);
-      });
+     return eventRepository.findById(id)
+     .map(e -> {
+      e.setEventDate(event.getEventDate());
+      e.setStartRegistrations(event.getStartRegistrations());
+      e.setEndRegistrations(event.getEndRegistrations());
+      e.setCapacity(event.getCapacity());
+      e.setCost(event.getCost());
+      e.setPrivateEvent(event.isPrivateEvent());
+      return eventRepository.save(e);
+     })
+     .orElseGet(() -> {
+       return eventRepository.save(event);
+     });
+   }
 
     public void deleteEvent(@PathVariable Long id) {
       eventRepository.deleteById(id);
     }
      
     public Event findEventById(long id) {
-      return eventRepository.findEventById(username);
+      return eventRepository.findEventById(id);
     }
 }
